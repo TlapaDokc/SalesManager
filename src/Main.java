266ea32d92b5a7a2744deff665afe35f029a10f1
Kuzin Dmitriy <SoraDnic@icloud.com>
2022-07-26 18:43:17 +0300

    Added branch longer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {23, 54, 12, 45, 52, 37, 44};
+        long[] sales = {23, 54, 12, 45, 52, 37, 44};
         SalesManager julyFirstWeek = new SalesManager(sales);
-        int maxSales = julyFirstWeek.max();
+        long maxSales = julyFirstWeek.max();
         System.out.println("Максимальное количество продаж за день: " + maxSales);
     }
 }
